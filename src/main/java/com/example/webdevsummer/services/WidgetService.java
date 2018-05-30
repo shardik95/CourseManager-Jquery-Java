e1b33ca8c2bd95736c16e1b6d81175fb44373308
shardik95 <shardik95@ccs.neu.edu>
2018-05-30 01:41:18 -0400

    apis done

--- src/main/java/com/example/webdevsummer/services/WidgetService.java
+++ src/main/java/com/example/webdevsummer/services/WidgetService.java
@@ -6,9 +6,11 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,7 +29,7 @@ public class WidgetService {
 	@Autowired
 	private TopicRepository topicRepository;
 	
-	@PostMapping("api/topic/{topicId}/widget/save")
+	@PostMapping("api/topic/{topicId}/widget")
 	public void saveAllWidgets(@PathVariable("topicId") int topicId,@RequestBody List<Widget> widgets) {
 		Optional<Topic> data=topicRepository.findById(topicId);
 		if(data.isPresent()) {
@@ -64,4 +66,36 @@ public class WidgetService {
 		return null;	
 	}
 	
+	@GetMapping("/api/widget/{widgetId}")
+	public Widget getWidget(@PathVariable("widgetId") int widgetId) {
+		Optional<Widget> data = widgetRepository.findById(widgetId);
+		if(data.isPresent()) {
+			Widget widget = data.get();
+			return widget;
+		}
+		return null;
+	}
+	
+	@PutMapping("/api/widget/{widgetId}")
+	public Widget updateWidget(@PathVariable("widgetId") int widgetId,@RequestBody Widget newWidget) {
+		Optional<Widget> data= widgetRepository.findById(widgetId);
+		if(data.isPresent()) {
+			Widget widget = data.get();
+			widget.setImageUrl(newWidget.getImageUrl());
+			widget.setLinkText(newWidget.getLinkText());
+			widget.setLinkUrl(newWidget.getLinkUrl());
+			widget.setListText(newWidget.getListText());
+			widget.setListType(newWidget.getListType());
+			widget.setParagraphText(newWidget.getParagraphText());
+			widget.setSize(newWidget.getSize());
+			widget.setText(newWidget.getText());
+			return widgetRepository.save(widget);
+		}
+		return null;
+	}
+	
+	@DeleteMapping("/api/widget/{widgetId")
+	public void deleteWidget(@PathVariable("widgetId") int widgetId) {
+		 widgetRepository.deleteById(widgetId);
+	}
 }
